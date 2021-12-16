bad9e62e0b0b67f9df1594c69c1bca40fab41c0b
めぐみん <59683877+AbnerHung@users.noreply.github.com>
2021-12-16 20:04:26 +0800

    Update inode.h

--- inode.h
+++ inode.h
@@ -15,10 +15,8 @@ class inode{
     int initiate_inode(string data, int datasize, disk& thedisk);
     vector<int> get_index();
     void to_index_table(vector<int> addresses, int blocknum);
-    int direct[DIRECT_NUM] = {-1};    //direct_num = 10
-    index_table_one table_one;
-    //index_table_two table_two;
-    //index_table_three table_three;
+    int direct[DIRECT_NUM] = {-1};
+    FirstLevelIndexBlock table_one;
 };
 int inode::initiate_inode(string data, int datasize, disk &thedisk) {
   //如果磁盘空间不够的话返回 0
@@ -40,38 +38,6 @@ vector<int> inode::get_index() { // 得到所有的 index
   for (int i = 0; i < table_one_index.size(); ++i) {
       tmp.push_back((table_one_index[i]));
   }
-
-  /*
-  int length = blocknum;
-
-  for (int i = 0; i < min(length, 10); i++) {
-    tmp.push_back(direct[i]); //如果小于10的话就直接用直接表
-  }
-  if (length > 10) {
-    // 10~22 都是一级索引
-    vector<int> table_one_index;
-    table_one_index = this->table_one.get_all_block_index();
-    for (int i = 0; i < table_one_index.size(); ++i) {
-      tmp.push_back((table_one_index[i]));
-    }
-  }
-  if (length > 23) {
-    // 23~191 都是二级索引
-    vector<int> table_two_index;
-    table_two_index = this->table_two.get_all_block_index();
-    for (int i = 0; i < table_two_index.size(); ++i) {
-      tmp.push_back((table_two_index[i]));
-    }
-  }
-  if (length > 192) {
-    // 192~ 都是一级索引
-    vector<int> table_three_index;
-    table_three_index = this->table_three.get_all_block_index();
-    for (int i = 0; i < table_three_index.size(); ++i) {
-      tmp.push_back((table_three_index[i]));
-    }
-  }
-   */
   return tmp;
 }
 //把他放到 index——table 里面
@@ -80,24 +46,4 @@ void inode::to_index_table(vector<int> addresses, int blocknum){
   for (int i = 0; i <  length; i++) {
       this->table_one.write(addresses[i]);
   }
-  /*
-  for (int i = 0; i < min(length, 10); i++) {
-      this->direct[i] = addresses[i];
-  }
-  if (length > 10){
-    for (int i = 10; i < min(23, length); i++) {
-      this->table_one.write(addresses[i]);
-    }
-  }
-  if (length > 23){
-    for (int i = 23; i < min(192, length); i++) {
-      this->table_two.write(addresses[i]);
-    }
-  }
-  if (length > 192){
-    for (int i = 192; i < min(BLOCKNUM, length); i++) {
-      this->table_three.write(addresses[i]);
-    }
-  }
-   */
 }
